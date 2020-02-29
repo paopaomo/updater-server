312fd1d1cd732b6c7dad68c43844c8e8537466af
Zi Ye <18612319597@163.com>
2020-02-29 19:43:22 +0800

    save crash file

--- router/crash.route.js
+++ router/crash.route.js
@@ -1,12 +1,32 @@
 const Router = require('koa-router');
-const router = new Router();
 const multer = require('koa-multer');
+const path = require('path');
+const writeFile = require('write-file');
 
-const uploadCrash = multer({ dest: 'crash/' });
+const router = new Router();
+
+const crashPath = path.join(__dirname, '../crash');
+
+const uploadCrash = multer({ dest: crashPath });
 
 router.post('/crash', uploadCrash.single('upload_file_minidump'), (ctx, next) => {
-    console.log(ctx.req.body);
-    // 存DB
+    const { body, file } = ctx.req;
+    const { filename } = file;
+    const report = {
+        ...body,
+        filename,
+        date: new Date()
+    };
+    const filePath = path.resolve(__dirname, `../crash/${filename}.json`);
+
+    writeFile(filePath, JSON.stringify(report), (err) => {
+        if(err) {
+            return console.error('Error Saving', report);
+        }
+        console.log('Crash Saved', filePath, report);
+    });
+
+    ctx.status = 200;
 });
 
 module.exports = router;
