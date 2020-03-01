80b1b62a55ae4b1e2ce85624f950189963829cab
Zi Ye <18612319597@163.com>
2020-03-01 12:00:10 +0800

    fire-sale update

--- helper/version.js
+++ helper/version.js
@@ -17,4 +17,20 @@ const getRemoteControlNewVersion = (version) => {
     return null;
 };
 
-module.exports = { getRemoteControlNewVersion };
+const getFireSaleNewVersion = (version) => {
+    if(!version) {
+        return null;
+    }
+    const maxVersion = {
+        name: '2.0.0',
+        pub_date: '2020-02-24T14:30:39.307Z',
+        notes: 'Fire Sale 新增功能 BBB',
+        url: 'http://127.0.0.1:8888/public/Mercurius-2.0.0-mac.zip'
+    };
+    if(compareVersions.compare(maxVersion.name, version, '>')) {
+        return maxVersion;
+    }
+    return null;
+};
+
+module.exports = { getRemoteControlNewVersion, getFireSaleNewVersion };
--- router/update.route.js
+++ router/update.route.js
@@ -1,11 +1,21 @@
 const Router = require('koa-router');
 const router = new Router();
-const { getRemoteControlNewVersion } = require('../helper/version');
+const { getRemoteControlNewVersion, getFireSaleNewVersion } = require('../helper/version');
 
 router.get('/remote-control/releases/darwin', (ctx, next) => {
     // 处理mac更新, ?version=1.0.0&uid=123
     const { version } = ctx.query;
     const newVersion = getRemoteControlNewVersion(version);
+    if(newVersion) {
+        ctx.body = newVersion;
+    } else {
+        ctx.status = 204;
+    }
+});
+
+router.get('/fire-sale/releases/darwin', (ctx, next) => {
+    const { version } = ctx.query;
+    const newVersion = getFireSaleNewVersion(version);
     if(newVersion) {
         ctx.body = newVersion;
     } else {
